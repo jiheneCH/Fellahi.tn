1123a4b37577e17b20963c3d5dbc3f21b15dd498
Jihene Chaalali <jihene.chaalali@esprit.tn>
2025-04-15 22:11:18 +0100

    commit gestion distribution integré avec user

---
+++ src/main/java/tn/esprit/pi/Entities/Role.java
@@ -0,0 +1,29 @@
+package tn.esprit.fallehiuser.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private RoleName roleName;  // Enum for role name (e.g., ADMIN, USER)
+
+    public RoleName getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(RoleName roleName) {
+        this.roleName = roleName;
+    }
+}
